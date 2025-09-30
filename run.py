451a13a53ee07a5b0695ed853e164edc689772be
proj5thit-cpu <proj5thit@gmail.com>
2025-10-01 00:36:12 +0400

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,12 @@ from app import create_app
 # Create the Flask app using the factory function
 app = create_app()
 
-
+# -----------------------------
+# Ensure app context is pushed
+# -----------------------------
+with app.app_context():
+    from app.routes import init_decision_tree
+    init_decision_tree(app)
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
